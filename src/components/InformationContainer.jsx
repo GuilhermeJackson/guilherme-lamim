5e1950c1935f2af2053b0736d8102140500dbeb1
guilherme <guilherme.lamim96@gmail.com>
2022-11-02 22:24:40 -0300

    creating sidebar w/p

---
+++ src/components/InformationContainer.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const InformationContainer = () => {
+    return <section id="information">
+            <div className="info-card">
+                
+                <div>
+                    <h3>Telefone</h3>
+                    <p>(47)999999-9999</p>
+                </div>
+            </div>
+            <div className="info-card">
+                <div>
+                    <h3>E-mail</h3>
+                    <p>guilherme.lamim96@gmail.com</p>
+                </div>
+            </div>
+            <div className="info-card">
+                <div>
+                    <h3>Localização</h3>
+                    <p>Blumenau - SC</p>
+                </div>
+            </div>
+        </section>
+    
+};
+
+export default InformationContainer
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Avatar from "../img/imageRandom.jpg"
+import SocialNetworks from './SocialNetworks'
+import InformationContainer from './InformationContainer'
+import "../styles/components/sidebar.sass"
+
+const Sidebar = () => {
+    return (
+        <aside id="sidebar">
+            <img src={Avatar} alt="Guilherme Jackson Lamim"></img>
+            <p className="title">Desenveolvelvle</p>
+            <SocialNetworks/>
+            <InformationContainer />
+            <a href="" className="btn">
+                Download currículo
+            </a>
+        </aside>
+    )
+}
+
+export default Sidebar
