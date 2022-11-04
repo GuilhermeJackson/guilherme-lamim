b87536cdf8426518eadd26ee6023c446551bb919
guilherme <guilherme.lamim96@gmail.com>
2022-11-04 11:55:26 -0300

    created sidebar

--- src/components/InformationContainer.jsx
+++ src/components/InformationContainer.jsx
@@ -1,28 +1,34 @@
 import React from "react";
+import { AiFillPhone, AiOutlineMail, AiFillEnvironment } from "react-icons/ai"
+import '../styles/components/informationcontainer.sass'
 
 const InformationContainer = () => {
     return <section id="information">
-            <div className="info-card">
+                <div className="info-card">
+                    <AiFillPhone id="phone-icon" />
+                    <div>
+                        <h3>Telefone</h3>
+                        <p>(47)999999-9999</p>
+                    </div>
+                </div>
                 
-                <div>
-                    <h3>Telefone</h3>
-                    <p>(47)999999-9999</p>
+                <div className="info-card">
+                    <AiOutlineMail id="email-icon" />
+                    <div>
+                        <h3>E-mail</h3>
+                        <p>guilherme.lamim96@gmail.com</p>
+                    </div>
                 </div>
-            </div>
-            <div className="info-card">
-                <div>
-                    <h3>E-mail</h3>
-                    <p>guilherme.lamim96@gmail.com</p>
+
+                <div className="info-card">
+                    <AiFillEnvironment id="pin-icon" />
+                    <div>
+                        <h3>Localização</h3>
+                        <p>Blumenau - SC</p>
+                    </div>
+
                 </div>
-            </div>
-            <div className="info-card">
-                <div>
-                    <h3>Localização</h3>
-                    <p>Blumenau - SC</p>
-                </div>
-            </div>
-        </section>
-    
+            </section>
 };
 
 export default InformationContainer
