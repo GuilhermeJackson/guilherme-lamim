1652810b2064512f44c47e0da8b0e15ff0259f2d
guilherme <guilherme.lamim96@gmail.com>
2022-11-07 04:05:56 -0300

    inserted personal data in component AboutContainer

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -3,12 +3,17 @@ import React from "react"
 const AboutContainer = () => {
     return <section>
         <h2>Sobre</h2>
-        <p> Lorem ipsum dolor sit amet. Sed magni error et molestias veritatis
-            eum quia laboriosam est perspiciatis perferendis eos consequatur autem 
-            ut vero internos. Sit sint sunt a voluptate maiores et illum laborum. 
-            Vel fuga enim ea aperiam nesciunt aut quod magnam. Cum nostrum commodi 
-            non voluptate impedit qui dolores esse aut nemo consequuntur qui galisum 
-            doloribus quo quibusdam cumque
+        <p> 
+            Olá, meu nome é Guilherme, sou graduado em Sistemas de Informação, trabalho como dev front-end e meu propósito 
+            é criar soluções modernas e acessíveis para facilitar a vida dos usuários.
+        </p>
+        <p>
+            Alguns dos meus conhecimentos: Angular, Typescript, HTML, CSS, testes unitários do JavaScript com Jasmine e 
+            Karma, testes de integração com Cypress, eCharts, Azure DevOps, GIT.
+        </p>
+        <p>    
+            Iniciei minha formação em Sistemas de Informação em 2017 onde tive meu primeiro contato com tecnologia; 
+            em 2019 no curso de desenvolvimento Android e iOS através da Proway, como parte do programa de formação da AmbevTech.
         </p>
     </section>
 };
