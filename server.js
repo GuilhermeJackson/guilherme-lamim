2bae0d686c3cf0e7b6284b4a818eaef5d94c4056
guilherme <guilherme.lamim96@gmail.com>
2022-11-08 17:17:46 -0300

    config express server

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-console.log('TESTE')
+const express = require('express')
+
+const { resolve } = require('path')
+
+const app = express()
+
+app.arguments('/',
+    express.static(
+        resolve(
+            __dirname,
+            './build'
+        )
+    )
+)
+
+app.listen(process.envi.PORT || 3000, (err) => {
+    if(err) { return console.log(err) }
+    console.log('Tudo funcionando')
+})
